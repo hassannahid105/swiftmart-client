b5993533fd28ad2e5dc35cda2fe2640d8d068315
nahidHassan405 <infosharemail405@gmail.com>
2024-10-18 19:23:51 +0600

    project stater

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      {/* outlet */}
+      <h2>main</h2>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default MainLayout;
