06a1e862a318f2279e27085872046e11979fcb51
nahidHassan405 <infosharemail405@gmail.com>
2024-10-25 21:52:18 +0600

    filter all jobs by category and descending oreder

--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import JobCard from "../../components/Tabs/JobCard";
 
 const AllJobs = () => {
-  const [itemsPerPage, setItemsPerPages] = useState(2);
+  const [itemsPerPage, setItemsPerPages] = useState(9);
   const [totalData, setTotalData] = useState(0);
   const [currentPages, setCurrentPages] = useState(1);
   const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("");
+  const [search, setSearch] = useState("");
   const numberOfPages = Math.ceil(totalData / itemsPerPage);
   const pages = [
     ...Array(numberOfPages)
@@ -19,12 +21,12 @@ const AllJobs = () => {
       const { data } = await axios(
         `${
           import.meta.env.VITE_lOCALHOST
-        }/all-jobs?page=${currentPages}&size=${itemsPerPage}&filter=${filter}`
+        }/all-jobs?page=${currentPages}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
       );
       setJobs(data);
     };
     getData();
-  }, [currentPages, itemsPerPage, filter]);
+  }, [currentPages, itemsPerPage, filter, sort]);
   useEffect(() => {
     const getCount = async () => {
       console.log(filter);
@@ -40,6 +42,16 @@ const AllJobs = () => {
   const handleCurrentPagesNumber = (pageNumber) => {
     setCurrentPages(pageNumber);
   };
+  const handleReset = () => {
+    setFilter("");
+    setSort("");
+  };
+  const handleSearch = (e) => {
+    e.preventDefault(); //
+    const search = e.target.search.value;
+    setSearch(search);
+  };
+  console.log(search);
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
       <div>
@@ -62,7 +74,7 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form>
+          <form onSubmit={handleSearch}>
             <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
               <input
                 className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
@@ -82,13 +94,16 @@ const AllJobs = () => {
               name="category"
               id="category"
               className="border p-4 rounded-md"
+              onChange={(e) => setSort(e.target.value)}
             >
               <option value="">Sort By Deadline</option>
               <option value="dsc">Descending Order</option>
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button className="btn" onClick={handleReset}>
+            Reset
+          </button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {jobs.map((job) => (
